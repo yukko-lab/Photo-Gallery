e85663061b8a76acd6c86f9d832c96df1ce8cd12
yukko-lab <107912444+yukko-lab@users.noreply.github.com>
2023-10-16 14:52:00 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const main = document.querySelector('#main-image');
 const thumbs = document.querySelectorAll('.thumb');
+const text = document.querySelector('#caption');
 
 // 確認
 //console.log(main);
@@ -22,6 +23,7 @@ const thumbs = document.querySelectorAll('.thumb');
 thumbs.forEach(function(item, index) {
   item.onmouseover = function() {
     main.src = this.dataset.image;
+    text.textContent = this.title;
   }
 });
 
